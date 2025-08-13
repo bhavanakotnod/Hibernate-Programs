5c28308db4070e8e204cb78a3c043a0943f6478f
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-08-13 21:39:13 +0530

    Add files via upload

---
+++ HibernateOne/src/main/java/com/main/DeleteData.java
@@ -0,0 +1,34 @@
+package com.main;
+
+
+	import org.hibernate.Session;
+	import org.hibernate.SessionFactory;
+	import org.hibernate.Transaction;
+	import org.hibernate.cfg.Configuration;
+
+	import com.entity.Student;
+
+	public class DeleteData {
+
+		public static void main(String[] args) {
+
+			Configuration cfg = new Configuration();
+			cfg.configure();
+			cfg.addAnnotatedClass(Student.class);
+
+			SessionFactory sf = cfg.buildSessionFactory();
+			Session ss = sf.openSession();
+			Transaction tr = ss.beginTransaction();
+			
+			int id = 1;
+			Student s1 = ss.get(Student.class, id);
+			
+			ss.delete(s1);
+			System.out.println("Data is deleted.....");
+			
+			tr.commit();
+			ss.close();
+			
+			
+		}
+	}
---
+++ HibernateOne/src/main/java/com/main/InsertData.java
@@ -0,0 +1,38 @@
+package com.main;
+
+
+	import org.hibernate.Session;
+	import org.hibernate.SessionFactory;
+	import org.hibernate.Transaction;
+	import org.hibernate.cfg.Configuration;
+
+	import com.entity.Student;
+
+	public class InsertData {
+
+		public static void main(String[] args) {
+
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			cfg.addAnnotatedClass(Student.class);
+
+			SessionFactory sf = cfg.buildSessionFactory();
+			Session ss = sf.openSession();
+			Transaction tr = ss.beginTransaction();
+
+			Student s = new Student();
+			s.setName("Maitri");
+			s.setCity("Hyd");
+			s.setAge(30);
+			s.setSurname("Giri");
+
+			ss.persist(s);
+			ss.close();
+
+			System.out.println("Data is inserted...");
+
+		}
+
+	}
+
+
